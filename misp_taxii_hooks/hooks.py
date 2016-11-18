3a588632dac4d0f7590af1741e0b084429172659
Hannah Ward <Hannah.ward9001@gmail.com>
2016-11-18 11:45:30 +0000

    OpenTAXII were nice and fixed their mySQL bug <3

--- misp_taxii_hooks/hooks.py
+++ misp_taxii_hooks/hooks.py
@@ -8,17 +8,19 @@ from opentaxii.signals import (
 
 ## CONFIG
 
-CONFIG = {
-            "MISP_URL" : "localhost",
-            "MISP_API" : "DEADBEEF",
-        }
-
-MISP = pymisp.PyMISP( 
-                        config["MISP_URL"],
-                        config["MISP_API"],
-                )
-
 def post_stix(manager, content_block, collection_ids, service_id):
+    CONFIG = {
+                "MISP_URL" : "localhost",
+                "MISP_API" : "DEADBEEF",
+            }
+
+    MISP = pymisp.PyMISP( 
+                            CONFIG["MISP_URL"],
+                            CONFIG["MISP_API"],
+                    )
+
+    with open("/tmp/test.txt", "w") as f:
+        f.write("connect!")
     print("Content: {}".format(content_block.content))
 
 CONTENT_BLOCK_CREATED.connect(post_stix)
