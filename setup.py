a69f5506a9087fb0735a8247e2961af8ac1aeb42
Hannah Ward <Hannah.ward9001@gmail.com>
2017-03-31 10:33:15 +0100

    fix: Typo in script name

--- setup.py
+++ setup.py
@@ -13,6 +13,6 @@ setup(
     author_email="hannah.ward2@baesystems.com",
     packages=['misp_taxii_hooks'],
     install_requires=["zmq", "misp-stix-converter", "pymisp>=2.4.53", "pyaml>=3.11", "cabby>=0.1", "mysqlclient>=1.3.9", "nose>=1.3.7"],
-    scripts=["scripts/start-misp-taxii.sh", "scripts/push_published_to_taxii.py", "scripts/install-remove-server.sh"]
+    scripts=["scripts/start-misp-taxii.sh", "scripts/push_published_to_taxii.py", "scripts/install-remote-server.sh"]
 )
 
